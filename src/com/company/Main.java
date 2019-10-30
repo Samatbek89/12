f1d77fba834172f98e038a545768ef8d2ee1c3d7
Samatbek89 <sambrero899@gmail.com>
2019-10-30 21:03:27 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        for (int i = 0; i < 107; i++) {
+            System.out.println(rand.nextInt(107));
+        }
+
+
+
+
+    }
+}
